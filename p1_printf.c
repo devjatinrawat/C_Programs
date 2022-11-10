4a004cc53937f33dad74464d7d69f32c3b39dc2e
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-10 23:11:06 +0530

    Updated!

---
+++ p1_printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+printf("india is great");
+getch();
+}
+
+
+// Main is the entry point of the program 
+// Printf() simple prints the text enclosed within the braces 
