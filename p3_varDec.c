eb7edb874422c73e1119d2fc090d32e64a803a47
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-10 23:12:26 +0530

    Updated!

---
+++ p3_varDec.c
@@ -0,0 +1,10 @@
+#include<stdio.h>  /*Standard input output header file*/
+#include<conio.h>  /*console input output header file*/
+void main()
+{
+int a;	/*a is a variable*/
+clrscr();  /*Clear screen header file required is conio.h*/
+a=10;
+printf("a=%d",a);
+getch();  /*Get character header file required is conio.h. It pauses the screen */
+}
