df3978d5419181a8be765fbf7372ab34bac75e26
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-10 23:13:54 +0530

    Updated!

---
+++ p5_simpleInterest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>  /*Standard input output header file*/
+#include<conio.h>  /*console input output header file*/
+main()
+{
+float p,r,t,i;
+clrscr();
+p=1034.0;
+r=4.5;
+t=3.0;
+i=p*r*t/100;
+printf("Principle=%f",p);
+printf("\nRate=%f",r);
+printf("\nTime=%f",t);
+printf("\nInterest=%f",i);
+getch();
+}
+
+
