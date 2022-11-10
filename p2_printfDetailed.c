4d47e6a590589a11d94cb6cc71d4cd624f033e84
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-10 23:11:56 +0530

    Updated!

---
+++ p2_printfDetailed.c
@@ -0,0 +1,19 @@
+#include<stdio.h>  //Standard input output header file
+#include<conio.h>  /*console input output header file*/
+void main()
+{
+clrscr();  /*Clear screen header file required is conio.h*/
+printf("india is great");
+getch();  /*Get character header file required is conio.h. It pauses the screen */
+}
+/*Console refers to screen
+( is known as opening parenthesis
+) is known as closing parenthesis
+{ is known as opening brace
+} is known as closing brace
+[ is known as opening bracket
+] is known as closing bracket
+< is known as opening  angular   brackets
+> is known as closing  angular   brackets
+
+*/
