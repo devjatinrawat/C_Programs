83463f38126ea8bbf1c7ef30976632cd96f5000a
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-12 21:46:51 +0530

    Updated!

---
+++ p6_temp.c
@@ -0,0 +1,15 @@
+
+
+#include<stdio.h>  /*Standard input output header file*/
+#include<conio.h>  /*console input output header file*/
+main()
+{
+float f,c;  /*Variable declaration*/
+clrscr();
+f=98.6;
+c=(f-32)/1.8;
+printf("\nTemperature in farenheit=%f",f);
+printf("\nTemperature in celcius=%f",c);
+getch();
+}
+
